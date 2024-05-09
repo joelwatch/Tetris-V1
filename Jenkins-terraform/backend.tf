423e51687962979eaa0398d5970369b5c3937bbc
pro <pro@pros-mbp.home>
2024-05-09 17:38:17 -0400

    update

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "project-registre" # Replace with your actual S3 bucket name
+    bucket = "jenkins-tetris" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "us-east-2"
   }
